30ead14f1e4da58496b74cae1635cd995ad57604
maira-ships-it <maira.farooq.mf@gmail.com>
2026-06-28 10:46:50 -0700

    feat(carousel): adds new svgs to Icon map

--- src/themis/Icon/Icon.tsx
+++ src/themis/Icon/Icon.tsx
@@ -1,9 +1,17 @@
 import chevronLeftCircle from '../icons/svgs/chevron-left-circle.svg'
 import chevronRightCircle from '../icons/svgs/chevron-right-circle.svg'
+import play from '../icons/svgs/play.svg'
+import pause from '../icons/svgs/pause.svg'
+import soundOn from '../icons/svgs/sound-on.svg'
+import soundOff from '../icons/svgs/sound-off.svg'
 
 const iconMap = {
   chevronLeftCircle,
   chevronRightCircle,
+  play,
+  pause,
+  soundOn,
+  soundOff,
 } as const
 
 export type IconName = keyof typeof iconMap
